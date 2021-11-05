97e1fd99788679f75900eecca40b413544cfb512
suo.li <suo.li@microfun.com>
2021-11-05 20:24:53 +0800

    init

---
+++ go.mod
@@ -0,0 +1,18 @@
+module golang-gin-app
+
+go 1.16
+
+require (
+	github.com/360EntSecGroup-Skylar/excelize v1.4.1 // indirect
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
+	github.com/astaxie/beego v1.12.3 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gin-gonic/gin v1.7.4 // indirect
+	github.com/go-ini/ini v1.63.2
+	github.com/gomodule/redigo v2.0.0+incompatible
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/swaggo/gin-swagger v1.3.3 // indirect
+	github.com/swaggo/swag v1.7.4
+	github.com/tealeg/xlsx v1.0.5 // indirect
+	github.com/unknwon/com v1.0.1 // indirect
+)
---
+++ routers/router.go
@@ -0,0 +1,66 @@
+package routers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	_ "golang-gin-app/docs"
+
+	"github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+
+	"golang-gin-app/middleware/jwt"
+	"golang-gin-app/resources/export"
+	"golang-gin-app/resources/setting"
+	"golang-gin-app/resources/upload"
+	"golang-gin-app/routers/api"
+	"golang-gin-app/routers/api/v1"
+)
+
+func InitRouter() *gin.Engine {
+	r := gin.New()
+
+	r.Use(gin.Logger())
+
+	r.Use(gin.Recovery())
+	gin.SetMode(setting.ServerSetting.RunMode)
+
+	r.StaticFS("/export", http.Dir(export.GetExcelFullPath()))
+	r.StaticFS("/upload/images", http.Dir(upload.GetImageFullPath()))
+
+	r.GET("/auth", api.GetAuth)
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.POST("/upload", api.UploadImage)
+
+	apiv1 := r.Group("/api/v1")
+	apiv1.Use(jwt.JWT())
+	{
+		//
+		apiv1.GET("/tags", v1.GetTags)
+		//
+		apiv1.POST("/tags", v1.AddTag)
+		//
+		apiv1.PUT("/tags/:id", v1.EditTag)
+		//
+		apiv1.DELETE("/tags/:id", v1.DeleteTag)
+		//
+		r.POST("/tags/export", v1.ExportTag)
+		//
+		r.POST("/tags/import", v1.ImportTag)
+
+		//
+		apiv1.GET("/articles", v1.GetArticles)
+		//
+		apiv1.GET("/articles/:id", v1.GetArticle)
+		//
+		apiv1.POST("/articles", v1.AddArticle)
+		//
+		apiv1.PUT("/articles/:id", v1.EditArticle)
+		//
+		apiv1.DELETE("/articles/:id", v1.DeleteArticle)
+
+	}
+
+	return r
+}
